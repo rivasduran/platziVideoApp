f7de84a50733508f85a123791e764b531a71848e
rivasduran <rivasduran84@gmail.com>
2019-06-05 20:18:55 -0500

    Hasta este punto empezamos a hacer las listas reutilizables del proyecto y el header

--- App.js
+++ App.js
@@ -1,53 +1,21 @@
 import React from 'react';
-import { Platform, StyleSheet, Text, View, Image } from 'react-native';
+import { 
+  Text 
+} from 'react-native';
 
-//Platform es el componente que nos ayuda a detectar si estamos en ios o en android
-
-//Image esta es la variable para importar imagenes
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Home from './src/screens/containers/home';
+import Header from './src/sections/components/header';
+import SuggestionList from './src/videos/containers/suggestion-list';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Image 
-          source={require('./assets/logo.png')}
-          style={{
-            width: 300,
-            height: 80
-          }}
-        />
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Home>
+        <Header />
+        <Text>Buscador</Text>
+        <Text>Categorias</Text>
+        <SuggestionList />
+      </Home>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Platform.select({
-      ios: 'green',
-      android: 'purple',
-    }),
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ src/videos/containers/suggestion-list.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {
+	FlatList,
+	Text,
+} from 'react-native';
+
+import Layout from '../components/suggestion-list-layout.js';
+import Empty from '../components/empty.js';
+import Separator from '../components/vertical-separator.js';
+
+class SuggestionList extends Component {
+	renderEmpty = () => <Empty text="No hay sugerencias :(" />
+	itemSeparator = () => <Separator />
+	render(){
+		const list = [
+			{
+				title: 'Advengers',
+				key: '1'
+			},
+			{
+				title: 'Pokemon',
+				key: '2'
+			}
+		]
+		return(
+			<Layout
+				title="Recomendado para ti"
+			>
+				<FlatList 
+					data={list}
+					//ListEmptyComponent={() => <Text>No hay elementos en la lista</Text>}
+					ListEmptyComponent={this.renderEmpty}
+					ItemSeparatorComponent={this.itemSeparator}
+					renderItem={({ item }) => <Text>{item.title}</Text> }
+				/>
+			</Layout>
+		)
+	}
+}
+
+
+export default SuggestionList;
